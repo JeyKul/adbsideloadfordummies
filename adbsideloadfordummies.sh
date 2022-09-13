7aff56f952a9e86528c19fc74b84c883d51b2222
JeyKul <77121325+JeyKul@users.noreply.github.com>
2022-09-13 23:40:29 +0200

    some sexy improvements
    thanks to Barry

--- adbsideloadfordummies.sh
+++ adbsideloadfordummies.sh
@@ -1,6 +1,28 @@
+#!/bin/bash
+
+echo checking some things
+
+if [[ $(which wget) ]]; then
+true
+DOWNLOAD=wget
+elif [[ $(which curl) ]]; then
+true
+DOWNLOAD=curl
+else
+echo "wget is not installed"
+exit 0
+fi
+
+if [[ $(which unzip) ]]; then
+true
+else 
+echo "unzip is not installed"
+exit 0
+fi
+
 mkdir ~/tmpp 
 cd ~/tmpp
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+$DOWNLOAD https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip
 cp -r platform-tools /usr/lib
 ln /usr/lib/platform-tools/adb /usr/bin
